288b5379d5ed7484de3d4499718603e267a1cdf4
朱云龙 <zhu_yunlong@czcg.com.cn>
2017-05-09 10:12:34 +0800

    初始化

---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '8.0'
+
+target 'YLBaseChat' do
+  # Uncomment the next line if you're using Swift or would like to use dynamic frameworks
+use_frameworks!
+
+  # Pods for YLBaseChat
+  
+pod 'RealmSwift', '~> 2.6.2'
+  
+
+end
---
+++ YLBaseChat/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  YLBaseChat
+//
+//  Created by yl on 17/5/9.
+//  Copyright © 2017年 yl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.red
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
