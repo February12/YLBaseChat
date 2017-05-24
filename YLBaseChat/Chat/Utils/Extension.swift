8779f856181aa42fa1e08bb1b3c83e71b0a5c62c
朱云龙 <zhu_yunlong@czcg.com.cn>
2017-05-24 15:21:43 +0800

    no message

---
+++ YLBaseChat/Chat/Utils/Extension.swift
@@ -0,0 +1,10 @@
+//
+//  Extension.swift
+//  YLBaseChat
+//
+//  Created by yl on 17/5/24.
+//  Copyright © 2017年 yl. All rights reserved.
+//
+
+import Foundation
+import UIKit
--- YLBaseChat/Chat/Utils/String+Extension.swift
+++ YLBaseChat/Chat/Utils/String+Extension.swift
@@ -11,7 +11,8 @@ import UIKit
 import YYText
 
 struct YLTextRange {
-    
+    var text:String?
+    var range:NSRange?
 }
 
 // MARK: - String 拓展
@@ -26,15 +27,33 @@ extension String {
             return nil;
         }
         
-//        var tmpImageArray = Array<YLTextRange>()
+        var tmpImageArray = Array<YLTextRange>()
         
-        NSRegularExpression().enumerateMatches(in: "\\[[a-zA-Z0-9\\u4e00-\\u9fa5]+\\]", options: NSRegularExpression.MatchingOptions.reportCompletion, range: NSRange(location: 0,length: 15)) { (result:NSTextCheckingResult?, flags:NSRegularExpression.MatchingFlags, stop) in
+        let regex = try! NSRegularExpression(pattern: "\\[[a-zA-Z0-9\\u4e00-\\u9fa5]+\\]", options: NSRegularExpression.Options(rawValue: 0))
+        
+        let regexArray = regex.matches(in: content, options: NSRegularExpression.MatchingOptions.reportProgress, range: NSRange(location: 0,length: content.characters.count))
+        
+        for result:NSTextCheckingResult in regexArray {
+        
+            let range = result.range
             
-            print("")
+            tmpImageArray.append(YLTextRange(text: content.substring(with: yl_range(range)!),range: range))
             
         }
-        
+            
+    
         return nil
+    }
+    
+    // NSRange -> Range<String.Index>
+    func yl_range(_ nsRange: NSRange) -> Range<String.Index>? {
+        guard
+            let from16 = utf16.index(utf16.startIndex, offsetBy: nsRange.location, limitedBy: utf16.endIndex),
+            let to16 = utf16.index(from16, offsetBy: nsRange.length, limitedBy: utf16.endIndex),
+            let from = String.Index(from16, within: self),
+            let to = String.Index(to16, within: self)
+            else { return nil }
+        return from..<to
     }
     
 }
