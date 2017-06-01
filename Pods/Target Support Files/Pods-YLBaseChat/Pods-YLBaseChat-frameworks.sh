e52612d395f6cd6dda4a89a6c5d1b85874855259
朱云龙 <zhu_yunlong@czcg.com.cn>
2017-06-01 14:33:24 +0800

    no message

--- Pods/Target Support Files/Pods-YLBaseChat/Pods-YLBaseChat-frameworks.sh
+++ Pods/Target Support Files/Pods-YLBaseChat/Pods-YLBaseChat-frameworks.sh
@@ -89,7 +89,6 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "$BUILT_PRODUCTS_DIR/ImagePickerSheetController/ImagePickerSheetController.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Realm/Realm.framework"
   install_framework "$BUILT_PRODUCTS_DIR/RealmSwift/RealmSwift.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SnapKit/SnapKit.framework"
@@ -97,7 +96,6 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/YYText/YYText.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "$BUILT_PRODUCTS_DIR/ImagePickerSheetController/ImagePickerSheetController.framework"
   install_framework "$BUILT_PRODUCTS_DIR/Realm/Realm.framework"
   install_framework "$BUILT_PRODUCTS_DIR/RealmSwift/RealmSwift.framework"
   install_framework "$BUILT_PRODUCTS_DIR/SnapKit/SnapKit.framework"
